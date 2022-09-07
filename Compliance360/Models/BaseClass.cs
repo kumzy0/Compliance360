bfcf2629b0935da8ed26b76e2e5877a5ce7decb7
Elijah Adewuyi <Eadewuyi@wragbysolutions.com>
2022-09-07 23:58:23 +0100

    Add project files.

---
+++ Compliance360/Models/BaseClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Compliance360.Models
+{
+  public class BaseClass
+  {
+    public DateTime DateCreated { get; set; }
+    public DateTime DateUpdated { get; set; }
+    public DateTime DateModified { get; set; }
+    public bool IsDeleted { get; set; }
+    public bool IsSaved { get; set; }
+
+    public BaseClass()
+    {
+      DateCreated = DateTime.Now;
+      DateUpdated = DateTime.Now;
+      DateModified = DateTime.Now;
+    }
+
+  }
+}
---
+++ Compliance360/Models/UserAuth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Compliance360.Models
+{
+  public class UserAuth : BaseClass
+  {
+    public string Id { get; set; }
+    public string UserId { get; set; }
+    public virtual ApplicationIdentity User { get; set; }
+    public string Email { get; set; }
+    public string OTP { get; set; }
+    public DateTime LastGenerated { get; set; }
+    public string Others { get; set; }
+
+  }
+}
