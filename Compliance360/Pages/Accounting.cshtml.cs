8f3b0d0daf396fecad45eaf9f5368b99289e5b04
Femi Olayode <folayode@wragbysolutions.com>
2022-09-11 11:54:07 +0100

    Audit trail, Annual report, filling annual report, Scuml application, create scuml application, accounting, setting, company, users

---
+++ Compliance360/Pages/Accounting.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Compliance360.Pages
+{
+    public class AccountingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Compliance360/Pages/CompanyDashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Compliance360.Pages
+{
+    public class CompanyDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
